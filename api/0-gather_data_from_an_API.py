6094312c8049a4871d3f03bba6bd202487a80a94
FatjonMileti <tonimileti@yahoo.com>
2022-12-02 08:33:11 -0800

    ...

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,14 +6,13 @@ if __name__ == '__main__':
     from requests import get
     from sys import argv
 
-    rq_1 = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
-    rq_users = get(rq_1).json()
+    rq_users = get('https://jsonplaceholder.typicode.com/users/{}'.
+                   format(argv[1])).json()
 
     rq_name = rq_users.get('name')
 
-    rq_2 = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1])
-    
-    rq_data =get(rq_2).json()
+    rq_data = get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                  format(argv[1])).json()
 
     done = total = 0
 
@@ -22,7 +21,8 @@ if __name__ == '__main__':
         if tasks.get('completed'):
             done += 1
 
-    print('Employee {} is done with tasks({}/{}):'.format(rq_name, done, total))
+    print('Employee {} is done with tasks({}/{}):'.
+          format(rq_name, done, total))
 
     for task in rq_data:
         if task.get('completed'):
