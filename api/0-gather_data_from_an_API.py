423b82cb3c7fd070d19197754c52ad90f34eab30
FatjonMileti <tonimileti@yahoo.com>
2022-12-02 09:12:48 -0800

    ...

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
 ''' *** *** '''
+from requests import get
+from sys import argv
 
 
-response_API = 'https://jsonplaceholder.typicode.com/'
 if __name__ == "__main__":
-    users = requests.get("{}users/{}".format(response_API, argv[1])).json()
-    todos = requests.get("{}todos?userId={}".format(
-        response_API, argv[1])).json()
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+    completed = 0
+    total = 0
+    tasks = []
+    response2 = get('https://jsonplaceholder.typicode.com/users/')
+    data2 = response2.json()
 
-    task_completed = [task['title'] for task in todos if task['completed']]
-    print("Employee {} is done with tasks({}/{}):"
-          .format(users["name"], len(task_completed), len(todos)))
-    print("\n".join("\t {}".format(done) for done in task_completed))
+    for i in data2:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
+
+    for i in data:
+        if i.get('userId') == int(argv[1]):
+            total += 1
+
+            if i.get('completed') is True:
+                completed += 1
+                tasks.append(i.get('title'))
+
+    print("Employee {} is done with tasks({}/{}):".format(employee,
+                                                          completed, total))
+
+    for i in tasks:
+        print("\t {}".format(i))
